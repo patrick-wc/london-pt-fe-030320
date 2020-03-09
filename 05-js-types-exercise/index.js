b43bec3cc0e2b6cd228924a2e0a75806d9201f38
Patrick Whitty-Clarke <hello@patrickwc.net>
2020-03-09 18:37:45 +0000

    completed exercises 05 and 07

---
+++ 05-js-types-exercise/index.js
@@ -0,0 +1,16 @@
+let greeting = "alright";
+let name = "patrick";
+
+let price = 20.5;
+let quantity = 5;
+let total = price * quantity;
+
+let age1 = 32;
+let age2 = 42;
+let age3 = 22;
+let averageAge = (age1 + age2 + age3) / 3;
+
+let loggedIn = true;
+let adminUser = false;
+
+let missingInfo = undefined;
--- 07-js-comparisons-and-conditionals-exercise/index.js
+++ 07-js-comparisons-and-conditionals-exercise/index.js
@@ -7,3 +7,27 @@ const passwordConfirmation = "JSisCool888";
 const sortOrder = "ALPHABETICAL";
 
 // ADD YOUR CODE BELOW
+if ( target == userValue ) {
+    targetMatchesCoercedUserValue = true;
+}
+
+if ( target === userValue ) {
+    targetMatchesUserValue = true;
+} else {
+    targetMatchesUserValue = false;
+}
+
+let passwordMessage;
+
+if ( password === passwordConfirmation ) {
+    passwordMatchesConfirmation = true;
+} else {
+    passwordMessage = "Oops, your password and confirmation do not match!";
+    passwordMatchesConfirmation = false;
+}
+
+let fruits;
+
+if ( sortOrder === 'ALPHABETICAL' ) {
+    fruits = "banana, cherry, watermelon";
+}
